69491436d10ade06fbaff9a75e20ab657f8010f5
Cal <calum.moore@gmail.com>
2022-08-31 12:13:55 +0100

    Use correct value for collections count

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import { Input, Stack, HStack, Box,  Container, VStack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
+import { CollectionMeta } from '@spacetimexyz/client'
+import { useSpacetime, useCollection } from '@spacetimexyz/react'
 import { Layout } from 'features/common/Layout'
 import { useApi } from 'features/common/useApi'
 import useInterval from 'use-interval'
 import { Stat } from './Stat'
 
 export function Home () {
+  const spacetime = useSpacetime()
   const api = useApi()
   const [block, setBlock] = useState('-')
 
@@ -15,6 +18,8 @@ export function Home () {
     const block = res.data?.sync_info?.latest_block_height ?? '-'
     setBlock(block)
   }, 1000, true)
+
+  const { data } = useCollection<CollectionMeta>(spacetime.collection('$collections'))
 
   return (
     <Layout>
@@ -30,7 +35,7 @@ export function Home () {
                 <HStack spacing={6}>
                   <Stat title='Validators' stat={4} />
                   <Link to='collections'>
-                    <Stat title='Collections' stat={1} />
+                    <Stat title='Collections' stat={data ? data?.length : '-'} />
                   </Link>
                 </HStack>
               </Stack>
