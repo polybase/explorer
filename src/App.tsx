4b158b4cc2df9206145179256908f11db6ba7d41
Cal <calum.moore@gmail.com>
2022-12-20 18:04:26 +0000

    Fix big int (#24)
    * Add bundle analyze
    
    * Remvoe unused web3 lib
    
    * Update browser list

--- src/App.tsx
+++ src/App.tsx
@@ -13,34 +13,30 @@ import ScrollToTop from 'modules/common/ScrollToTop'
 import PostHogPageView from 'modules/common/PostHogPageView'
 import { ApiProvider } from 'features/common/ApiProvider'
 import { AuthProvider } from 'features/users/AuthProvider'
-import { Web3Provider } from 'features/web3/Web3Provider'
 import { PolybaseProvider } from '@polybase/react'
 import polybase from 'config/polybase'
-import web3 from 'config/web3'
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY ?? '')
 
 export const App = () => {
   return (
-    <Web3Provider web3={web3}>
-      <PolybaseProvider polybase={polybase}>
-        <AuthProvider
-          domain={process.env.REACT_APP_DOMAIN}
-          storagePrefix={process.env.REACT_APP_AUTH_STORAGE_PREFIX}
-        >
-          <ApiProvider baseURL={process.env.REACT_APP_API_URL}>
-            <Elements stripe={stripePromise}>
-              <ChakraProvider theme={theme}>
-                <Router>
-                  <PostHogPageView />
-                  <ScrollToTop />
-                  <AppRoutes />
-                </Router>
-              </ChakraProvider>
-            </Elements>
-          </ApiProvider>
-        </AuthProvider>
-      </PolybaseProvider>
-    </Web3Provider>
+    <PolybaseProvider polybase={polybase}>
+      <AuthProvider
+        domain={process.env.REACT_APP_DOMAIN}
+        storagePrefix={process.env.REACT_APP_AUTH_STORAGE_PREFIX}
+      >
+        <ApiProvider baseURL={process.env.REACT_APP_API_URL}>
+          <Elements stripe={stripePromise}>
+            <ChakraProvider theme={theme}>
+              <Router>
+                <PostHogPageView />
+                <ScrollToTop />
+                <AppRoutes />
+              </Router>
+            </ChakraProvider>
+          </Elements>
+        </ApiProvider>
+      </AuthProvider>
+    </PolybaseProvider>
   )
 }
--- src/features/web3/useWeb3.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { Web3ProviderContext } from './Web3Provider'
-
-export function useWeb3 () {
-  return useContext(Web3ProviderContext)
-}
