41b42117679feae6d2486fdc60591265b62707a7
prllrp <patricklane09@gmail.com>
2023-01-10 09:19:09 -0600

    Fix linting

--- src/features/common/Pagination.tsx
+++ src/features/common/Pagination.tsx
@@ -1,4 +1,7 @@
-import { Button, Stack, Box, HStack, Container} from "@chakra-ui/react";
+import { Button, Stack, Box, HStack, Container } from '@chakra-ui/react';
+
+
+/** @jsx Pagination */
 
 export interface PaginationProps {
     page: number,
@@ -9,24 +12,24 @@ export interface PaginationProps {
 
 export default function Pagination({ page, setPage, pageLength, items }: PaginationProps) {
 
-    if(page*pageLength - items.length > pageLength) {
-        setPage(page-1)
-    }
-    const pageItems =  items.slice(pageLength * (page - 1), pageLength * page)
-    
-    return (
-            
-            <Container maxW='container.md' p={4} alignSelf={'center'} >
-                <Stack spacing={4}>
-                    {pageItems}
-                </Stack>
-                <Container maxW='container.md' p={4}  >
-                    <HStack width={'max'} spacing={4} justify={'center'}>
-                    <Button onClick={()=>setPage(page - 1)} > {"<"} </Button>
-                    <Box>{page}</Box>
-                    <Button onClick={() => setPage(page + 1)}> {">"} </Button>
-                    </HStack>
-                </Container>
-            </Container>   
-    );
-    }
+  if (page * pageLength - items.length > pageLength) {
+    setPage(page - 1);
+  }
+  const pageItems = items.slice(pageLength * (page - 1), pageLength * page);
+
+  return (
+
+    <Container maxW='container.md' p={4} alignSelf={'center'} >
+      <Stack spacing={4}>
+        {pageItems}
+      </Stack>
+      <Container maxW='container.md' p={4} >
+        <HStack width={'max'} spacing={4} justify={'center'}>
+          <Button onClick={() => setPage(page - 1)} > {'<'} </Button>
+          <Box>{page}</Box>
+          <Button onClick={() => setPage(page + 1)}> {'>'} </Button>
+        </HStack>
+      </Container>
+    </Container>
+  );
+}
