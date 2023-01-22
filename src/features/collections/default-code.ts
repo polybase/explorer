1b2537892785a7f12fdb1a15a83be31a3d079bf3
Cal <calum.moore@gmail.com>
2023-01-22 00:08:43 +0000

    Update collection default code (#40)
    * Update collection default code
    
    * Add mandatory and optional prop

--- src/features/collections/default-code.ts
+++ src/features/collections/default-code.ts
@@ -1,27 +1,30 @@
-export const DEFAULT_CODE = (name: string) => `// This is a default collection deployed using the explorer.
-// You should edit these collection rules for your use case.
+export const DEFAULT_CODE = (name: string) => `// This is an example collection definition.
+// You should edit it for your use case.
 
-// The language is very similar to JavaScript, but keep in
-// mind that semi-colons are mandatory!
+// The language (Polylang) is similar to JavaScript,
+// but semi-colons are mandatory.
 
+// The \`collection\` keyword defines a named collection.
+// Collection properties define the "columns" in a record.
 collection ${name} {
-  // id is required on all collections
-  // id MUST be unique
+  // \`id\` is unique and required on all collections
   id: string;
 
-  // Add a public key of the owner
-  // of the record, we can then use this 
-  // public key to implement permissions
+  // We will use a public key to authenticate function
+  // calls later
   publicKey: string;
 
-  // An optional property denoted with ?
-  name?: string; 
+  // A mandatory property
+  name: string; 
 
-  // Constructor is called when a new record is
-  // created, make sure to assign a value to this.id
+  // An optional property denoted with ?
+  age?: number; 
+
+  // \`constructor\` is called when a new record is
+  // created, make sure to assign a value to \`this.id\`
   constructor (id: string) {
-    // this.id MUST be assigned in the constructor 
-    // this.id MUST be unique
+    // \`this.id\` must be assigned in the constructor
+    // \`this.id\` must be unique in collection
     this.id = id;
     
     // You can assign the publicKey of the user who is
@@ -30,12 +33,12 @@ collection ${name} {
     this.publicKey = ctx.publicKey;
   }
 
-
   // You can add your own functions to determine rules
   // on who can update the records data
   function setName (name: string) {
+    // Check if the caller is the original creator of the record.
     if (ctx.publicKey != this.publicKey) {
-      error('You are not the owner');
+      error('You are not the creator of this record.');
     }
     this.name = name;
   }
