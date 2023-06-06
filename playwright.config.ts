dd190adb66850d0def204120fcd8196a40ae6e02
GlitchGoddessQA <christester67@gmail.com>
2023-06-06 16:53:45 +0200

    change script in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn',
+    command: 'yarn start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 150000,
