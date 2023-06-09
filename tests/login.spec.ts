00acd356aa5f3e1fbb2ba9b2f04c26d173caeba7
GlitchGoddessQA <christester67@gmail.com>
2023-06-09 15:06:45 +0200

    finished login tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 import { faker } from '@faker-js/faker'
 import { checkErrorToast, common, waitForPageLoaded } from './utils/commmon'
 import { getCodeForSignIn } from './utils/email'
-import { fillEmailInput, login, openCodeEnteringStep, openLoginEmailModal } from './selectors/login.selectors'
+import { fillCodeInput, fillEmailInput, login, openCodeEnteringStep, openLoginEmailModal, registerUI } from './selectors/login.selectors'
 
 test.describe('home page + login screen', async () => {
   test.beforeEach(async ({ page }) => {
@@ -12,6 +12,7 @@ test.describe('home page + login screen', async () => {
   })
 
   test('home page with all necessary elements opened', async ({ page }) => {
+    // Assert
     await expect(page.locator('h2[test-id="root-hash"]')).toBeVisible()
     await expect(page.locator('h2[test-id="collection-amount"]')).toBeVisible()
     page.waitForSelector('a[href="https://docs.polybase.xyz"]')
@@ -20,7 +21,10 @@ test.describe('home page + login screen', async () => {
   })
 
   test('when change the theme, expected colors to be updated', async ({ page }) => {
+    // Arrange
     const attribute = async() => await page.locator('html').getAttribute('data-theme')
+
+    // Act & Assert
     expect(await attribute()).toEqual('light')
 
     await page.locator('[aria-label="Switch to dark mode"]').click()
@@ -30,93 +34,155 @@ test.describe('home page + login screen', async () => {
     expect(await attribute()).toEqual('light')
   })
 
-  test('check the metamask link', async ({ page }) => {
+  test('when open login modal, expect metamask link is displayed', async ({ page }) => {
+    // Arrange
     await login.loginBtn(page).click()
     const iframe = await login.getLoginModalContent(page)
     const link = await iframe!.getByRole('link' , { name: 'Install Metamask' }).getAttribute('href')
+
+    // Assert
     expect(link).toContain('https://chrome.google.com/webstore/detail/metamask')
   })
 
-  test('login with empty email field', async ({ page }) => {
+  test('when login with empty email field, expected validation to be displayed', async ({ page }) => {
+    // Arrange
     const iframe = await openLoginEmailModal(page)
-    await iframe!.getByRole('button', { name: 'Continue' }).click()
+
+    // Act
+    await login.continueModalBtn(iframe!).click()
     await iframe!.waitForSelector('[id^="toast"]')
 
-    checkErrorToast(iframe!, 'Invalid email address')
+    // Assert
+    await checkErrorToast(iframe!, 'Invalid email address')
   })
 
-  test('login with invalid format of email', async ({ page }) => {
+  test('when login with invalid format of email, expected validation to be displayed', async ({ page }) => {
+    // Arrange
     const iframe = await openLoginEmailModal(page)
+
+    // Act
     await fillEmailInput(iframe!, '12345')
     await iframe!.waitForSelector('[id^="toast"]')
 
-    checkErrorToast(iframe!, 'Invalid email address')
+    // Assert
+    await checkErrorToast(iframe!, 'Invalid email address')
   })
 
-  test('login with empty code input', async ({ page }) => {
+  test('when login with empty code input, expected validation to be displayed', async ({ page }) => {
+    // Arrange
     const fakeEmail = faker.internet.userName() + '@mailto.plus'
     const iframe = await openLoginEmailModal(page)
     await openCodeEnteringStep(iframe!, fakeEmail)
-    await iframe!.getByRole('button', { name: 'Continue' }).click()
 
-    checkErrorToast(iframe!, 'Invalid code, must be 6 numbers')
+    // Act
+    await login.continueModalBtn(iframe!).click()
+
+    // Assert
+    await checkErrorToast(iframe!, 'Invalid code, must be 6 numbers')
   })
 
-  test('login with invalid format of the verification code (less than 6 digits)', async ({ page }) => {
+  test('when login with invalid format of the verification code (less than 6 digits), expected validation to be displayed', async ({ page }) => {
+    // Arrange
     const fakeEmail = faker.internet.userName() + '@mailto.plus'
     const iframe = await openLoginEmailModal(page)
     await openCodeEnteringStep(iframe!, fakeEmail)
-    await iframe!.locator('input').fill('12345')
-    await iframe!.getByRole('button', { name: 'Continue' }).click()
 
-    checkErrorToast(iframe!, 'Invalid code, must be 6 numbers')
+    // Act
+    await fillCodeInput(iframe!, '12345')
+
+    // Assert
+    await checkErrorToast(iframe!, 'Invalid code, must be 6 numbers')
   })
 
-  test('login with invalid format of the verification code (more than 6 digits)', async ({ page }) => {
+  test('when login with invalid format of the verification code (more than 6 digits), expected validation to be displayed', async ({ page }) => {
+    // Arrange
     const fakeEmail = faker.internet.userName() + '@mailto.plus'
     const iframe = await openLoginEmailModal(page)
     await openCodeEnteringStep(iframe!, fakeEmail)
-    await iframe!.locator('input').fill('1234567')
-    await iframe!.getByRole('button', { name: 'Continue' }).click()
 
-    checkErrorToast(iframe!, 'Invalid code, must be 6 numbers')
+    // Act
+    await fillCodeInput(iframe!, '1234567')
+
+    // Assert
+    await checkErrorToast(iframe!, 'Invalid code, must be 6 numbers')
   })
 
-  test('login with invalid format of the verification code (letters)', async ({ page }) => {
+  test('when login with invalid format of the verification code (letters), expected validation to be displayed', async ({ page }) => {
+    // Arrange
     const fakeEmail = faker.internet.userName() + '@mailto.plus'
     const iframe = await openLoginEmailModal(page)
     await openCodeEnteringStep(iframe!, fakeEmail)
-    await iframe!.locator('input').fill('abcdef')
-    await iframe!.getByRole('button', { name: 'Continue' }).click()
 
-    checkErrorToast(iframe!, 'Invalid code, must be 6 numbers')
+    // Act
+    await fillCodeInput(iframe!, 'abcdef')
+
+    // Assert
+    await checkErrorToast(iframe!, 'Invalid code, must be 6 numbers')
   })
 
-  test('login with invalid verification code', async ({ page }) => {
+  test('when login with invalid verification code, expected error to be displayed', async ({ page }) => {
+    // Arrange
     const fakeEmail = faker.internet.userName() + '@mailto.plus'
     const iframe = await openLoginEmailModal(page)
     await openCodeEnteringStep(iframe!, fakeEmail)
-    await iframe!.locator('input').fill('123456')
-    await iframe!.getByRole('button', { name: 'Continue' }).click()
 
-    checkErrorToast(iframe!, 'Email code is invalid or has expired')
+    // Act
+    await fillCodeInput(iframe!, '123456')
+
+    // Assert
+    await checkErrorToast(iframe!, 'Email code is invalid or has expired')
   })
 
-  test('successful login with email', async ({ page, request }) => {
+  test('when login with email, expected to be logged in', async ({ page, request }) => {
+    // Arrange
     const fakeEmail = faker.internet.userName() + '@mailto.plus'
     const iframe = await openLoginEmailModal(page)
+
+    // Act & Assert
     await fillEmailInput(iframe!, fakeEmail)
-
     await iframe!.waitForSelector(`:text("Enter the code sent to ${fakeEmail}")`)
     await common.wait(4000)
-
     const code = await getCodeForSignIn(request, fakeEmail)
-    await iframe!.locator('input').fill(code)
-    await iframe!.getByRole('button', { name: 'Continue' }).click()
-    waitForPageLoaded(page)
+    await fillCodeInput(iframe!, code)
     await iframe!.waitForSelector(':text("The app is requesting for you to sign the following request.")')
-    await iframe!.getByRole('button', { name: 'Sign' }).click()
-    waitForPageLoaded(page)
+    await login.signModalBtn(iframe!).click()
+    await waitForPageLoaded(page)
+
+    expect(login.logoutBtn(page)).toBeVisible()
+  })
+
+  test('when logout and allow access to the account, expect to be logged in', async ({ page, request }) => {
+    // Arrange
+    const fakeEmail = faker.internet.userName() + '@mailto.plus'
+    await registerUI({ page, fakeEmail, request })
+    await login.logoutBtn(page).click()
+    await login.loginBtn(page).click()
+
+    // Act
+    const newIframe = await login.getLoginModalContent(page)
+    await newIframe!.waitForSelector(':text("You are logged in using email as:")')
+    await newIframe!.waitForSelector(`:text("${fakeEmail}")`)
+    await newIframe!.getByRole('button', { name: 'Allow Access' }).click()
+    await login.signModalBtn(newIframe!).click()
+    await waitForPageLoaded(page)
+
+    // Assert
     await page.waitForSelector('button:text("Logout")')
+  })
+
+  test('when logout and swap account, expected to be logged out', async ({ page, request }) => {
+    // Arrange
+    const fakeEmail = faker.internet.userName() + '@mailto.plus'
+    await registerUI({ page, fakeEmail, request })
+    await login.logoutBtn(page).click()
+    await login.loginBtn(page).click()
+    const newIframe = await login.getLoginModalContent(page)
+    await newIframe!.waitForSelector(`:text("${fakeEmail}")`)
+
+    // Act
+    await newIframe!.getByRole('link', { name: 'Swap Account' }).click()
+
+    // Assert
+    await newIframe!.waitForSelector('a:text("Email")')
   })
 })
--- tests/selectors/login.selectors.ts
+++ tests/selectors/login.selectors.ts
@@ -1,5 +1,12 @@
-import { Frame, Page } from '@playwright/test'
-import { waitForPageLoaded } from '../utils/commmon'
+import { APIRequestContext, Frame, Page } from '@playwright/test'
+import { common, waitForPageLoaded } from '../utils/commmon'
+import { getCodeForSignIn } from '../utils/email'
+
+interface RegisterUI {
+  page: Page,
+  fakeEmail: string,
+  request: APIRequestContext
+}
 
 export const login = {
   loginBtn(page: Page) {
@@ -9,6 +16,15 @@ export const login = {
     const modal = await page.waitForSelector('#polybase-auth-modal-iframe')
     const iframe = await modal.contentFrame()
     return iframe
+  },
+  logoutBtn(page: Page) {
+    return page.locator('button:text("Logout")')
+  },
+  continueModalBtn(iframe: Frame) {
+    return iframe.getByRole('button', { name: 'Continue' })
+  },
+  signModalBtn(iframe: Frame) {
+    return iframe.getByRole('button', { name: 'Sign' })
   },
 }
 
@@ -29,7 +45,26 @@ export const openLoginEmailModal = async(page: Page) => {
   }
 }
 
+export const fillCodeInput = async(iframe: Frame, code: string) => {
+  await iframe.locator('input').fill(code)
+  await login.continueModalBtn(iframe).click()
+  await waitForPageLoaded(iframe)
+}
+
 export const openCodeEnteringStep = async(iframe: Frame, email: string) => {
   await fillEmailInput(iframe, email)
   await iframe!.waitForSelector(':text("Enter email code")')
+}
+
+export const registerUI = async({ page, fakeEmail, request }: RegisterUI) => {
+  const iframe = await openLoginEmailModal(page)
+  await fillEmailInput(iframe!, fakeEmail)
+  await common.wait(4000)
+
+  const code = await getCodeForSignIn(request, fakeEmail)
+  await iframe!.locator('input').fill(code)
+  await iframe!.getByRole('button', { name: 'Continue' }).click()
+  waitForPageLoaded(page)
+  await iframe!.getByRole('button', { name: 'Sign' }).click()
+  waitForPageLoaded(page)
 }
