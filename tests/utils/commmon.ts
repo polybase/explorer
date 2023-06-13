9251453fa2a22e045e22a2f5bb73c0ebc78a3fb4
GlitchGoddessQA <christester67@gmail.com>
2023-06-13 19:04:57 +0200

    add path check func

--- tests/utils/commmon.ts
+++ tests/utils/commmon.ts
@@ -1,4 +1,5 @@
 import { Frame, Locator, Page, expect } from '@playwright/test'
+import { baseENV } from '../config/config'
 
 const wait = (time: number) => new Promise(resolve => setTimeout(resolve, time))
 
@@ -15,24 +16,29 @@ const theme = {
   },
 }
 
-export const selectors = {
+export const elements = {
   toast(page: Page | Frame): Locator {
     return page.locator('[id^="toast-"]').first()
   },
   toastDesc(page: Page | Frame): Locator {
     return page.locator('[id^="toast-"][id*="description"]')
   },
+  menu(page: Page, name: string) {
+    return page.getByRole('link', { name }).first()
+  },
 }
 
 export const checkErrorToast = async(page: Page | Frame, text: string) => {
-  const background = await selectors.toast(page).evaluate(el => window.getComputedStyle(el).backgroundColor)
+  const background = await elements.toast(page).evaluate(el => window.getComputedStyle(el).backgroundColor)
   expect(background).toEqual(common.theme.red)
 
-  const toastDescription = selectors.toastDesc(page)
+  const toastDescription = elements.toastDesc(page)
   const textContent = await toastDescription.textContent()
   expect(textContent).toEqual(text)
 }
 
 export const waitForPageLoaded = async(page: Page | Frame) => page.waitForLoadState('load')
+
+export const pathNameShouldMatchRoute = async(page: Page, path: string) => expect(page.url()).toEqual(baseENV + path)
 
 export const common = { wait, inteceptRequests, theme }
