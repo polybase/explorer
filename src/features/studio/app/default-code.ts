1dff517a0b10382711f3eeb2aaa4736e12f604af
mateuszmlc <110990416+mateuszmlc@users.noreply.github.com>
2023-06-20 11:58:03 +0200

    Fix missing assignment for name in default collection code (#103)
    * Fix missing assignment for name in default collection code
    
    * Update default-code.ts

--- src/features/studio/app/default-code.ts
+++ src/features/studio/app/default-code.ts
@@ -28,10 +28,12 @@ collection User {
 
   // \`constructor\` is called when a new record is
   // created, make sure to assign a value to \`this.id\`
-  constructor (id: string) {
+  constructor (id: string, name: string) {
     // \`this.id\` must be assigned in the constructor
     // \`this.id\` must be unique in collection
     this.id = id;
+
+    this.name = name;
     
     // You can assign the publicKey of the user who is
     // creating the record, this can then be used to
